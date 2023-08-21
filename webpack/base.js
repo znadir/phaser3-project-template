4324e4671b004cf2c7649e6ff193ababf88dc9dc
Nadir <2266923@bdeb.qc.ca>
2023-08-21 18:13:48 -0400

    bundle more files

--- webpack/base.js
+++ webpack/base.js
@@ -4,37 +4,50 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
-    }),
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ]
+	entry: "./src/index.js",
+	mode: "development",
+	devtool: "eval-source-map",
+	devServer: {
+		port: 3000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: [/\.vert$/, /\.frag$/],
+				use: "raw-loader",
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg|xml|ttf|ogg|mp3|wav|m4a)$/i,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[ext]",
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin({
+			root: path.resolve(__dirname, "../"),
+		}),
+		new webpack.DefinePlugin({
+			CANVAS_RENDERER: JSON.stringify(true),
+			WEBGL_RENDERER: JSON.stringify(true),
+		}),
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+		}),
+	],
 };
--- webpack/prod.js
+++ webpack/prod.js
@@ -2,26 +2,36 @@ const merge = require("webpack-merge");
 const path = require("path");
 const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(base, {
-  mode: "production",
-  output: {
-    filename: "bundle.min.js"
-  },
-  devtool: false,
-  performance: {
-    maxEntrypointSize: 900000,
-    maxAssetSize: 900000
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
+	mode: "production",
+	output: {
+		filename: "bundle.min.js",
+	},
+	devtool: false,
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+		],
+	},
+	plugins: [new MiniCssExtractPlugin()],
+	performance: {
+		maxEntrypointSize: 900000,
+		maxAssetSize: 900000,
+	},
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					output: {
+						comments: false,
+					},
+				},
+			}),
+		],
+	},
 });
